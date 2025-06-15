ca2b032d70229b0677d2e26bb1543e49c37a803f
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2025-06-15 15:01:08 +0000

    Update baseUrl in Docusaurus config for root deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://nschristopher.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/agentic-development-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
